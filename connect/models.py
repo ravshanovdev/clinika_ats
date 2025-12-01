1a92ede08e557e6056c3a651c7d794da278d6bd8
sauron <kravshanov97@gmail.com>
2025-12-01 16:48:38 +0500

    added a few changes

--- connect/models.py
+++ connect/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class Review(models.Model):
     username = models.CharField(max_length=150)
-    description = models.TextField(max_length=550)
+    description = models.TextField()
     grade = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
 
     def __str__(self):
         return f"username: {self.username}"
