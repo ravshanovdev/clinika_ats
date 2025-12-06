78e26c2ecdf1ec57affb2b309de60d9216e21032
sauron <kravshanov97@gmail.com>
2025-12-06 14:27:14 +0500

    added

--- config/settings.py
+++ config/settings.py
@@ -154,6 +154,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "https://brosmedmax.pythonanywhere.com",
     "http://localhost:3000",
+    "https://brosmed.netlify.app"
 
 ]
 
