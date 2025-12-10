49d8ff983e5ef7b1672003432568cd1b944ed10e
sauron <kravshanov97@gmail.com>
2025-12-10 13:19:41 +0500

    added a few changes

--- connect/admin.py
+++ connect/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import SendMessage, TgAdmin, InfoContact, Review
-from modeltranslation.admin import TranslationAdmin
 
 admin.site.register([InfoContact,SendMessage, TgAdmin, Review])
 
